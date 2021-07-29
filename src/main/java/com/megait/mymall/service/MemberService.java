606fe65d3ba29bea25f3529cdf72b0befe358c45
Sera Lee <ssellu.lee@gmail.com>
2021-07-29 15:19:22 +0900

    0728

--- src/main/java/com/megait/mymall/service/MemberService.java
+++ src/main/java/com/megait/mymall/service/MemberService.java
@@ -9,10 +9,18 @@ import com.megait.mymall.validation.SignUpForm;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -40,27 +48,47 @@ public class MemberService {
                 .joinedAt(LocalDateTime.now())
 //                .emailCheckToken(UUID.randomUUID().toString())
                 .build();
+
+        // JPA Respository로부터 리턴된 Entity 객체는 영속 상태임
+        Member newMember = memberRepository.save(member);
+
+        // 회원 인증 이메일 전송
+        sendEmail(newMember);
+
+        // 새로 추가된 회원 (Member 엔티티)를 return
+        return newMember;
+    }
+
+    private void sendEmail(Member member) {
         member.generateEmailCheckToken();
-        memberRepository.save(member);
-
-        // TODO 이메일에 인증 링크 전송
-
         String url = "http://127.0.0.1:8080/email-check-token?token="
-                + member.getEmailCheckToken() + "&email=" +member.getEmail();
-        log.info("url : {}", url);
-
+                + member.getEmailCheckToken() + "&email=" + member.getEmail();
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(member.getEmail());
         message.setFrom("admin@mymall.com");
         message.setSubject("[mymall] 회원가입 이메일 인증 링크입니다.");
         message.setText("다음 링크를 클릭해주세요. =>" + url);
         consoleMailSender.send(message);
-
-        // 새로 추가된 회원 (Member 엔티티)를 return
-        return member;
     }
 
     public void login(Member member) {
-        // 해당 member를 authenticated 상태로 저장 => 로그인 완료 처리
+
+        Collection<GrantedAuthority> authorities =
+                List.of(new SimpleGrantedAuthority(member.getType().name()));
+
+        // "ROLE_USER" , "ROLE_ADMIN", "ROLE_WRITER"
+        // authorities : { new SGA("ROLE_USER"), new SGA("ROLE_ADMIN"), new SGA("ROLE_WRITER") }
+
+        // Username(=principal) 과 Password(=credencial) 를 가지고
+        // 스프링 시큐리티에게 인증을 요청할 때 사용하는 token
+        UsernamePasswordAuthenticationToken token =
+                new UsernamePasswordAuthenticationToken(
+                        /*username*/ member.getEmail(),
+                        /*password*/ member.getPassword(),
+                        /*authorities*/ authorities);
+
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(token);
+
     }
 }
